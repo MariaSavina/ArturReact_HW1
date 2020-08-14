ec1b50220cc70cf81ce98b8968d42ddbbb4660d2
MariaSavina <61714371+MariaSavina@users.noreply.github.com>
2020-08-14 15:57:09 +0300

    Add files via upload

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,6 +14,27 @@ import FackeLoading from "./FakeLoading";
 import Notification from "./Notification";
 import StopWatch from "./StopWatch";
 import Resizer from "./Resizer";
+import UserList from "./UserList";
+import GithubUsers from "./GithubUsers";
+import RandomAdvice from "./RandomAdvice";
+import ParentRandom from "./PrentRandom";
+import SearchingAdvice from "./SearchingAdvice";
+// import Form from "./Form";
+
+const users = [
+  {
+    name: "Alex",
+    id: 1,
+  },
+  {
+    name: "John",
+    id: 2,
+  },
+  {
+    name: "Bob",
+    id: 3,
+  },
+];
 
 function App() {
   return (
@@ -40,14 +61,23 @@ function App() {
         {/* <Greeting name="Petia" /> */}
         {/* <AgeDetector age={560} /> */}
         {/* <Counter step={3} initialCount={1000} /> */}
-        <h2>Facke Loading</h2>
-        <FackeLoading delay={3000} message="sucсsesы" />
-        <h2>Notification</h2>
-        <Notification delay={3000}></Notification>
-        <h2>StopWatch</h2>
-        <StopWatch />
-        <h2>Resizer</h2>
-        <Resizer ratio={0.15} />
+        {/* <h2>Facke Loading</h2> */}
+        {/* <FackeLoading delay={3000} message="sucсsesы" /> */}
+        {/* <h2>Notification</h2> */}
+        {/* <Notification delay={3000}></Notification> */}
+        {/* <h2>StopWatch</h2> */}
+        {/* <StopWatch /> */}
+        {/* <h2>Resizer</h2> */}
+        {/* <Resizer ratio={0.15} /> */}
+        {/* <Form /> */}
+        <h2>UserList</h2>
+        <UserList initialUsers={users} />
+        <h2>RandomAdvice</h2>
+        <ParentRandom />
+        <h2>GithubUsers</h2>
+        <GithubUsers />
+        <h2>Searching advice</h2>
+        <SearchingAdvice />
       </header>
     </div>
   );
