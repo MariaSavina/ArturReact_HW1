44f66de9207b59f3bce9d6cc89b746154c3c42a0
Maria <maria.savina1701@gmail.com>
2020-09-16 19:53:46 +0300

    Updates

---
+++ precache-manifest.4758b9b24ce136af76251effe049792a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15ec0d05b41a734376d6626c06e9a3a6",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "96b8998842cf6fe63a85",
+    "url": "/react-app/static/css/main.b74239e6.chunk.css"
+  },
+  {
+    "revision": "adf95367399e6f40fcf0",
+    "url": "/react-app/static/js/2.f1bc00b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.f1bc00b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b8998842cf6fe63a85",
+    "url": "/react-app/static/js/main.20a4ea96.chunk.js"
+  },
+  {
+    "revision": "e2c6b0a16d423a796dce",
+    "url": "/react-app/static/js/runtime-main.9a102076.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
